962b140add0ae475a38fd04a79afa1a09b901b2a
tbee10 <tbee10@gmail.com>
2022-07-25 18:38:23 -0600

    initial commit

---
+++ src/components/Compose/Compose.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import ProfileIcon from 'react-icons/lib/md/person-outline';
+import { titleIs } from 'selenium-webdriver/lib/until';
+
+import './Compose.css';
+
+//////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
+
+export default class Compose extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      text: ''
+    };
+
+    this.createPost = this.createPost.bind( this );
+  }
+
+  updateText( text ) {
+    this.setState({ text });
+  }
+
+  createPost() {
+    const { text } = this.state;
+    const { createPostFn } = this.props;
+
+    createPostFn( text );
+    this.setState({ text: ''});
+
+  }
+
+  render() {
+    // Destructuring
+    const { text } = this.state;
+
+    return (
+      <section className="Compose__parent">
+        <div className="Compose__top">
+
+          <div className="Compose__profile-picture">
+            <ProfileIcon />
+          </div>
+
+          {/* This is where you type the message for your new post */}
+          <input className="Compose__input"
+                 placeholder="What's on your mind?"
+                 value={ text }
+                 onChange={ ( e ) => this.updateText( e.target.value ) } />
+
+        </div>
+
+        <div className="Compose__bottom">
+          <button onClick={ this.createPost }>Compose</button>
+        </div>
+      </section>
+    )
+  }
+}
